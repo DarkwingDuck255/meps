9d6256121a113544997c624b1224f83d73c714f3
DarkwingDuck255 <arsen081190@yandex.ru>
2023-05-03 16:05:31 +0300

    новая попытка

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ function Header() {
     const currentUrl = useLocation();
     const [menuLang, setMenuLang] = React.useState(false);
     // const [currentLang, setCurrentLang] = React.useState('');
-
+// localStorage.setItem('lang', 'ru')
 
     function openLangMenu() {
         setMenuLang(true)
@@ -25,46 +25,75 @@ function Header() {
 
     function changeLangEN() {
         // setCurrentLang('en')
-        localStorage.setItem('lang', JSON.stringify(lang = 'en'))
+        localStorage.setItem('lang', 'en')
         // console.log(currentLang)
         console.log(localStorage.getItem('lang'))
     }
 
 
     function changeLangUZ() {
-        // setCurrentLang('uz')
+        localStorage.setItem('lang', 'uz')
         // console.log(currentLang)
+        console.log(localStorage.getItem('lang'))
     }
 
 
     return (
+
+        // <section className='header__wrapp'>
+        //     <div className="header">
+        //         <Link to='/' className='header__logo-link' >
+        //             <img className="header__logo" src={logo} alt="Логотип компании MEPS"></img>
+        //         </Link>
+        //         <nav className="header__nav">
+        //             <Link className={`header__nav-link common__link ${currentUrl.pathname === "/" ? 'common__current-link' : ''}`} to='/'>
+        //                 {localStorage.getItem('lang') === 'en' ? english.headerMenuAboutUs : 'О нас'}
+        //             </Link>
+        //             <Link className={`header__nav-link common__link  ${currentUrl.pathname === '/partnership' ? 'common__current-link' : ''}`} to='/partnership'>
+        //                 {localStorage.getItem('lang') === 'en' ? english.headerMenuPartnership : 'Партнерство'}
+        //             </Link>
+        //             <Link className={`header__nav-link common__link ${currentUrl.pathname === '/machinery' ? 'common__current-link' : ''}`} to='/machinery'>
+        //                 {localStorage.getItem('lang') === 'en' ? english.headerMenuEquipment : 'Оборудование'}
+        //             </Link>
+        //             <Link className={`header__nav-link common__link ${currentUrl.pathname === '/services' ? 'common__current-link' : ''}`} to='/services'>{localStorage.getItem('lang') === 'en' ? english.headerMenuServices : 'Услуги'}
+
+        //             </Link>
+        //             <Link className={`header__nav-link common__link ${currentUrl.pathname === '/contacts' ? 'common__current-link' : ''}`} to='/contacts'>
+        //                 {localStorage.getItem('lang') === 'en' ? english.headerMenuContacts : 'Контакты'}
+        //             </Link>
+        //         </nav>
         <section className='header__wrapp'>
             <div className="header">
                 <Link to='/' className='header__logo-link' >
                     <img className="header__logo" src={logo} alt="Логотип компании MEPS"></img>
                 </Link>
                 <nav className="header__nav">
-                    <Link className={`header__nav-link common__link ${currentUrl.pathname === "/" ? 'common__current-link' : ''}`} to='/'>
-                        {localStorage.getItem('lang') === 'en' ? english.headerMenuAboutUs : 'О нас'}
-                    </Link>
+                    <Link className={`header__nav-link common__link ${currentUrl.pathname === "/" ? 'common__current-link' : ''}`} to='/'>О нас</Link>
                     <Link className={`header__nav-link common__link  ${currentUrl.pathname === '/partnership' ? 'common__current-link' : ''}`} to='/partnership'>Партнерство</Link>
                     <Link className={`header__nav-link common__link ${currentUrl.pathname === '/machinery' ? 'common__current-link' : ''}`} to='/machinery'>Оборудование</Link>
                     <Link className={`header__nav-link common__link ${currentUrl.pathname === '/services' ? 'common__current-link' : ''}`} to='/services'>Услуги</Link>
-                    {/* <a className="header__nav-link common__link" href='#'>Контакты</a> */}
+                    <Link className={`header__nav-link common__link ${currentUrl.pathname === '/contacts' ? 'common__current-link' : ''}`} to='/contacts'>Контакты</Link>
                 </nav>
 
-                <div className='header__lang' onMouseOver={openLangMenu} onMouseLeave={closeLangMenu}>
+
+                <div className={`header__lang ${localStorage.getItem(lang) === 'en' ? '' : 'header__lang_visible'}`} onMouseOver={openLangMenu} onMouseLeave={closeLangMenu}>
                     <img src={russian} className='header__lang-russian' />
-                    <p className='header__lang-text'>RU</p>
+                    <p className='header__lang-text'>Rus</p>
                 </div>
+
+                <div className={`header__lang ${localStorage.getItem(lang) === 'en' ? 'header__lang_visible' : ''}`} onMouseOver={openLangMenu} onMouseLeave={closeLangMenu}>
+                    <img src={russian} className='header__lang-russian' />
+                    <p className='header__lang-text'>En</p>
+                </div>
+
                 <div className={`header__change-lang-menu ${menuLang ? 'header__change-lang-menu_visible' : ''}`} onMouseLeave={closeLangMenu} onMouseOver={openLangMenu}>
-                    <div className='header__change-lang-menu__option' onClick={changeLangEN}>
+                    <div className={`header__change-lang-menu__option ${localStorage.getItem(lang) === 'en' ? 'header__change-lang-menu__option_invisible' : ''}`} onClick={changeLangEN}>
                         <img src={russian} className='header__lang-russian' />
-                        <p className='header__lang-text'>EN</p>
+                        <p className='header__lang-text'>En</p>
                     </div>
-                    <div className='header__change-lang-menu__option' onClick={changeLangUZ}>
+                    <div className={`header__change-lang-menu__option ${localStorage.getItem(lang) === 'uz' ? 'header__change-lang-menu__option_invisible' : ''}`} onClick={changeLangUZ}>
                         <img src={russian} className='header__lang-russian' />
-                        <p className='header__lang-text'>UZ</p>
+                        <p className='header__lang-text'>Uz</p>
                     </div>
                     {/* <div className='header__change-lang-menu__option'>
                         <img src={russian} className='header__lang-russian' />
